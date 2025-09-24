05254a8b6316d35a18623f85df81fbb50f64c3fa
nikhiljagtap9 <niksjagtap9@gmail.com>
2025-09-24 20:17:14 +0530

    changed feed api fecth function

--- app/routes/api.scrapper.feed.$feed_id.tsx
+++ app/routes/api.scrapper.feed.$feed_id.tsx
@@ -43,42 +43,107 @@ import { json } from "@remix-run/node";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { db } from "~/lib/db.server";
 
+// function jsonToXml(content: any): string {
+//   const channel = content.channel;
+//    let xml = `<?xml version="1.0" encoding="utf-8"?>\n`;
+//   xml += `<rss version="2.0"\n`;
+//   xml += `     xmlns:dc="http://purl.org/dc/elements/1.1/"\n`;
+//   xml += `     xmlns:media="http://search.yahoo.com/mrss/">\n`;
+//   xml += `  <channel>\n`;
+
+//   xml += `    <title>${channel.title}</title>\n`;
+//   xml += `    <description>${channel.description}</description>\n`;
+//   xml += `    <link>${channel.link}</link>\n`;
+//   xml += `    <lastBuildDate>${channel.lastBuildDate}</lastBuildDate>\n`;
+//   xml += `    <language>${channel.language}</language>\n`;
+
+//   if (channel.image) {
+//     xml += `    <image>\n`;
+//     xml += `      <url>${channel.image.url}</url>\n`;
+//     xml += `      <title>${channel.image.title}</title>\n`;
+//     xml += `      <link>${channel.image.link}</link>\n`;
+//     xml += `    </image>\n`;
+//   }
+
+//   for (const item of channel.items || []) {
+//     xml += `    <item>\n`;
+//     xml += `      <title>${item.title}</title>\n`;
+//     xml += `      <description>${item.description}</description>\n`;
+//     xml += `      <link>${item.link}</link>\n`;
+//     if (item.guid) {
+//       xml += `      <guid isPermaLink="${item.guid.isPermaLink}">${item.guid.value}</guid>\n`;
+//     }
+//     if (item["dc:creator"]) {
+//       xml += `      <dc:creator>${item["dc:creator"]}</dc:creator>\n`;
+//     }
+//     xml += `      <pubDate>${item.pubDate}</pubDate>\n`;
+//     if (item["media:content"]) {
+//       xml += `      <media:content url="${item["media:content"].url}" />\n`;
+//     }
+//     xml += `    </item>\n`;
+//   }
+
+//   xml += `  </channel>\n</rss>`;
+//   return xml;
+// }
+
+function escapeXml(unsafe: string = ""): string {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
 function jsonToXml(content: any): string {
   const channel = content.channel;
-   let xml = `<?xml version="1.0" encoding="utf-8"?>\n`;
+  let xml = `<?xml version="1.0" encoding="utf-8"?>\n`;
   xml += `<rss version="2.0"\n`;
   xml += `     xmlns:dc="http://purl.org/dc/elements/1.1/"\n`;
   xml += `     xmlns:media="http://search.yahoo.com/mrss/">\n`;
   xml += `  <channel>\n`;
 
-  xml += `    <title>${channel.title}</title>\n`;
-  xml += `    <description>${channel.description}</description>\n`;
-  xml += `    <link>${channel.link}</link>\n`;
-  xml += `    <lastBuildDate>${channel.lastBuildDate}</lastBuildDate>\n`;
-  xml += `    <language>${channel.language}</language>\n`;
+  xml += `    <title>${escapeXml(channel.title)}</title>\n`;
+  xml += `    <description>${escapeXml(channel.description)}</description>\n`;
+  xml += `    <link>${escapeXml(channel.link)}</link>\n`;
+  if (channel.lastBuildDate) {
+    xml += `    <lastBuildDate>${escapeXml(channel.lastBuildDate)}</lastBuildDate>\n`;
+  }
+  if (channel.language) {
+    xml += `    <language>${escapeXml(channel.language)}</language>\n`;
+  }
 
   if (channel.image) {
     xml += `    <image>\n`;
-    xml += `      <url>${channel.image.url}</url>\n`;
-    xml += `      <title>${channel.image.title}</title>\n`;
-    xml += `      <link>${channel.image.link}</link>\n`;
+    xml += `      <url>${escapeXml(channel.image.url)}</url>\n`;
+    xml += `      <title>${escapeXml(channel.image.title)}</title>\n`;
+    xml += `      <link>${escapeXml(channel.image.link)}</link>\n`;
     xml += `    </image>\n`;
   }
 
   for (const item of channel.items || []) {
     xml += `    <item>\n`;
-    xml += `      <title>${item.title}</title>\n`;
-    xml += `      <description>${item.description}</description>\n`;
-    xml += `      <link>${item.link}</link>\n`;
+    xml += `      <title>${escapeXml(item.title)}</title>\n`;
+    xml += `      <description>${escapeXml(item.description)}</description>\n`;
+    if (item.link) {
+      xml += `      <link>${escapeXml(item.link)}</link>\n`;
+    }
     if (item.guid) {
-      xml += `      <guid isPermaLink="${item.guid.isPermaLink}">${item.guid.value}</guid>\n`;
+      xml += `      <guid isPermaLink="${item.guid.isPermaLink}">${escapeXml(
+        item.guid.value
+      )}</guid>\n`;
     }
     if (item["dc:creator"]) {
-      xml += `      <dc:creator>${item["dc:creator"]}</dc:creator>\n`;
+      xml += `      <dc:creator>${escapeXml(item["dc:creator"])}</dc:creator>\n`;
     }
-    xml += `      <pubDate>${item.pubDate}</pubDate>\n`;
+    if (item.pubDate) {
+      xml += `      <pubDate>${escapeXml(item.pubDate)}</pubDate>\n`;
+    }
     if (item["media:content"]) {
-      xml += `      <media:content url="${item["media:content"].url}" />\n`;
+      xml += `      <media:content url="${escapeXml(
+        item["media:content"].url
+      )}" />\n`;
     }
     xml += `    </item>\n`;
   }
@@ -86,6 +151,8 @@ function jsonToXml(content: any): string {
   xml += `  </channel>\n</rss>`;
   return xml;
 }
+
+
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const { feed_id } = params;

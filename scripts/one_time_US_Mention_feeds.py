f33c66748e65a8039a602c6598cc7f2ef7509bda
nikhiljagtap9 <niksjagtap9@gmail.com>
2025-10-01 20:09:03 +0530

    us mention log file overright

--- scripts/one_time_US_Mention_feeds.py
+++ scripts/one_time_US_Mention_feeds.py
@@ -29,7 +29,11 @@ USER_AGENTS = [
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[logging.FileHandler("us_mentions.log"), logging.StreamHandler(sys.stdout)],
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler("us_mentions.log", mode="w", encoding="utf-8"),
+        logging.StreamHandler(sys.stdout),
+    ],
 )
 
 # ----------------------------
